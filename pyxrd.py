04d9f863c5a393f4d3e74c231e9afdcdc087fc52
Shanyao-HEU <44898705+Shanyao-HEU@users.noreply.github.com>
2018-11-17 17:22:49 +0800

    Update pyxrd.py
    Use class to define my function, with which make it more beautiful

--- pyxrd.py
+++ pyxrd.py
@@ -7,105 +7,135 @@ import os
 import csv
 
 
-def find_start_row(csv_file):
-    csv_file = open(csv_file, "r")
-    reader = csv.reader(csv_file)
-    skip_row = 0
-    for item in reader:
-        if item[0] == 'Anode material':
-            anode = item[1]
+class FindXRD(object):
 
-        if item[0] == 'Scan step size':
-            step_size = item[1]
+    def __init__(self, path, theta_lim=[10, 80]):
+        self.path = path
+        self.theta_lim = theta_lim
 
-        if item[0] == 'Angle':
-            break
-        skip_row += 1
+    def find_all_csv(self):
 
-    return skip_row, anode, step_size
+        files = os.listdir(self.path)
+        csv_files = []
+        for file in files:
+            if file.endswith(".csv"):
+                csv_files.append(os.path.join(self.path, file))
 
+        return csv_files
 
-def setup_data(file_path):
-    # 读取文件夹下所有的csv文件并汇总
-    file_path = "test_data"
-    files = os.listdir(file_path)
-    csv_files = []
-    for file in files:
-        if file.endswith(".csv"):
-            csv_files.append(file)
+    def csv_to_df(self):
 
-    skip_row, anode, step_size = find_start_row(os.path.join(file_path, csv_files[0]))
+        csv_files = self.find_all_csv()
+        skip_row, anode, step_size = self.find_start_row()
 
-    df1 = pd.read_csv(os.path.join(file_path, csv_files[0]), skiprows=skip_row)
+        df1 = pd.read_csv(csv_files[0], skiprows=skip_row)
 
-    if len(csv_files) >= 1:
-        for file in csv_files[1:]:
-            filename = os.path.join(file_path, file)
-            print(filename)
-            df2 = pd.read_csv(filename, skiprows=26)
-            df1 = pd.concat([df1, df2], axis=1)
+        if len(csv_files) >= 1:
+            for file in csv_files[1:]:
+                print("Processing successful in {}".format(file))
+                df2 = pd.read_csv(file, skiprows=26)
+                df1 = pd.concat([df1, df2], axis=1)
 
-    return df1
+        return df1
 
+    def find_proper_lim(self, df, theta_lim, tol=0.1):
 
-def find_proper_lim(df, theta_lim, step_size):
+        low_index = df[np.abs(df.iloc[:, 0] - theta_lim[0]) < tol].index[0]
+        high_index = df[np.abs(df.iloc[:, 0] - theta_lim[1]) < tol].index[0]
 
-    low_index = df[np.abs(df.iloc[:, 0] - theta_lim[0])<step_size].index[0]
-    high_index = df[np.abs(df.iloc[:, 0] - theta_lim[1])<step_size].index[0]
+        df = df.iloc[low_index:high_index, :]
 
-    df = df.iloc[low_index:high_index, :]
+        return df
 
-    return df
+    def find_start_row(self):
+        csv_file = self.find_all_csv()[0]
+        csv_file = open(csv_file, "r")
+        reader = csv.reader(csv_file)
+        skip_row, anode, step_size = 0, "cu", 0.02
+        for item in reader:
+            if item[0] == 'Anode material':
+                anode = item[1]
 
+            if item[0] == 'Scan step size':
+                step_size = item[1]
 
-def plot_subplot_xrd(df, theta_lim, step_size, ignore_intensity=True):
-    num = int(df.columns.shape[0] / 2)
+            if item[0] == 'Angle':
+                break
+            skip_row += 1
 
-    df = find_proper_lim(df, theta_lim, step_size)
+        return skip_row, anode, step_size
 
-    for i in range(num):
-        plt.subplot(num, 1, i + 1)
-        theta = df.iloc[:, 2 * i]
-        intensity = df.iloc[:, 2 * i + 1]
+    def plot_subplot_xrd(self, save_fig="", ignore_intensity=True):
 
-        plt.plot(theta, intensity)
+        df = self.csv_to_df()
+        num = int(df.columns.shape[0] / 2)
+        df = self.find_proper_lim(df, self.theta_lim)
+        for i in range(num):
+            plt.subplot(num, 1, i + 1)
+            theta = df.iloc[:, 2 * i]
+            intensity = df.iloc[:, 2 * i + 1]
 
-        if ignore_intensity:
-            plt.yticks([])
-        plt.xlim([20, 80])
-        plt.ylim([np.max(intensity)*(-0.5), np.max(intensity)*1.2])
+            plt.plot(theta, intensity)
+
+            if ignore_intensity:
+                plt.yticks([])
+            plt.xlim([20, 80])
+            plt.ylim([np.max(intensity)*(-0.5), np.max(intensity)*1.2])
+
+            plt.ylabel("Intensity(a.u.)")
+        plt.xlabel(r"2$\theta$")
+        plt.show()
+        if save_fig:
+            plt.savefig(save_fig+".png")
+
+    def plot_total_xrd(self, df, ignore_intensity=True):
+        num = int(df.columns.shape[0] / 2)
+
+        df = self.find_proper_lim(df, self.theta_lim)
+        plt.figure()
+        for i in range(num):
+            theta = df.iloc[:, 2 * i]
+            intensity = df.iloc[:, 2 * i + 1] + np.max(df.iloc[:, 2*i]) * 1
+
+            plt.plot(theta, intensity)
+
+            if ignore_intensity:
+                plt.yticks([])
+
+        plt.xlim(self.theta_lim)
+        #plt.ylim([np.max(intensity)*(-0.2), np.max(intensity)*1.2])
 
         plt.ylabel("Intensity(a.u.)")
-    plt.xlabel(r"2$\theta$")
-    plt.show()
+        plt.xlabel(r"2$\theta$")
+        plt.show()
 
+    def plot_onebyone(self, save_fig="", ignore_intensity=True):
 
-def plot_total_xrd(df, theta_lim, step_size, ignore_intensity=True):
-    num = int(df.columns.shape[0] / 2)
+        df = self.csv_to_df()
+        num = int(df.columns.shape[0] / 2)
+        df = self.find_proper_lim(df, self.theta_lim)
+        for i in range(num):
+            plt.figure(figsize=(8, 8))
 
-    df = find_proper_lim(df, theta_lim, step_size)
-    plt.figure()
-    for i in range(num):
-        theta = df.iloc[:, 2 * i]
-        intensity = df.iloc[:, 2 * i + 1] + np.max(df.iloc[:, 2*i]) * 1
+            theta = df.iloc[:, 2 * i]
+            intensity = df.iloc[:, 2 * i + 1]
 
-        plt.plot(theta, intensity)
+            plt.plot(theta, intensity)
 
-        if ignore_intensity:
-            plt.yticks([])
+            if ignore_intensity:
+                plt.yticks([])
+            plt.xlim(self.theta_lim)
+            plt.ylim([np.max(intensity)*(-0.5), np.max(intensity)*1.2])
 
-    plt.xlim(theta_lim)
-    #plt.ylim([np.max(intensity)*(-0.2), np.max(intensity)*1.2])
-
-    plt.ylabel("Intensity(a.u.)")
-    plt.xlabel(r"2$\theta$")
-    plt.show()
+            plt.ylabel("Intensity(a.u.)")
+        plt.xlabel(r"2$\theta$")
+        plt.show()
+        if save_fig:
+            plt.savefig(save_fig+".png")
 
 
 if __name__ == "__main__":
-    #plot_subplot_xrd(setup_data("test_data"))
-
-    plot_total_xrd(setup_data("test_data"), [10, 80], 0.02)
-
-
+    xrd = FindXRD("2018.11.2hayao/cu")
+    #xrd.plot_subplot_xrd()
+    xrd.plot_onebyone()
 
